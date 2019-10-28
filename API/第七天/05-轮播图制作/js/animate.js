b1451cd8e6fce643f22934b50c7083609b2cc420
qqccyv <313187515@qq.com>
2019-10-28 21:44:25 +0800

    轮播图原理

---
+++ API/第七天/05-轮播图制作/js/animate.js
@@ -0,0 +1,13 @@
+function animate(obj, target, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+        var step = (target - obj.offsetLeft) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        if (obj.offsetLeft == target) {
+            clearInterval(obj.timer);
+            callback();
+        } else {
+            obj.style.left = obj.offsetLeft + step + 'px';
+        }
+    }, 15)
+}
