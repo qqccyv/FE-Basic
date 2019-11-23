305b8173c8d934f5c4d497e409a13f076362c694
qqccyv <313187515@qq.com>
2019-11-23 22:59:18 +0800

    删除 node_modules

--- 服务器编程-练习/node基础/代码/gulp-demo/node_modules/include/lib/includes/modules/lib/baz.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-
-var values = {
-  path: path.resolve(__dirname, path.basename(__filename, '.js')),
-  name: path.basename(__filename, '.js'),
-};
-
-module.exports = values;
