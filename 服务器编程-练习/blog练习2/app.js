444cfc1fc25bfd48bfc9effb5465e286d5b0f2ad
qqccyv <313187515@qq.com>
2019-11-23 21:25:45 +0800

    登录拦截功能和路由优化

--- 服务器编程-练习/blog练习2/app.js
+++ 服务器编程-练习/blog练习2/app.js
@@ -4,10 +4,8 @@ const path = require('path')
 const home = require('./route/home')
 const admin = require('./route/admin')
 const bodyPaser = require('body-parser')
-
-require('./model/connect')
-    // const User = require('./model/user')
 const session = require('express-session')
+require('./model/connect')
 app.use(session({
     secret: 'secret key',
     saveUninitialized: false,
@@ -21,6 +19,7 @@ app.engine('html', require('express-art-template'))
 app.engine('art', require('express-art-template'))
 app.use(bodyPaser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/admin', require('./middleware/loginGuard'))
 app.use('/home', home)
 app.use('/admin', admin);
 // app.on('request', (req, res) => {
--- 服务器编程-练习/blog练习2/route/admin.js
+++ 服务器编程-练习/blog练习2/route/admin.js
@@ -1,42 +1,12 @@
 const express = require('express');
 const admin = express.Router();
-const { User } = require('../model/user')
-const bcrypt = require('bcryptjs');
-admin.get('/login', (req, res) => {
-    res.render('admin/login.html')
-})
-admin.get('/error', (req, res) => {
+// const { User } = require('../model/user')
+// const bcrypt = require('bcryptjs');
+admin.get('/login', require('./admin/loginPage'))
 
-})
-admin.post('/login', async(req, res) => {
-    const { email, password } = req.body;
-    if (email.trim().length == 0 || password.trim().length == 0) {
-        return res.status(400).render('admin/error', { msg: '邮件地址或者密码错误' })
-    }
-    let user = await User.findOne({ email: email })
-    if (user) {
-        let isValid = await bcrypt.compare(password, user.password)
-        if (isValid) {
-            req.session.username = user.username;
-            res.redirect('/admin/user')
-        } else {
-            res.status(400).render('admin/error', { msg: '邮件地址或者密码错误' })
-        }
-    } else {
-        res.status(400).render('admin/error', { msg: '邮件地址或者密码错误' })
-    }
-})
+admin.post('/login', require('./admin/login'))
 
-// admin.get('/user', (req, res) => {
-//     res.render('admin/user', {
-//         msg: req.session.username
-//     })
-// })
+
 admin.get('/user', require('./admin/userPage'));
-admin.get('/logout', (req, res) => {
-    req.session.destroy(function() {
-        res.clearCookie('connect.sid')
-        res.redirect('/admin/login')
-    })
-})
+admin.get('/logout', require('./admin/logout'))
 module.exports = admin
